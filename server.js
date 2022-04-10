2570ea7803b2ee7291a7707476a5ab43c3c59689
JCIII <jcostello711@gmail.com>
2022-04-09 21:51:45 -0400

    made changes to server.js to enable sessions

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const routes = require('./controllers');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -17,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const routes = require('./controllers');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
