7345d8cc20e5c3e827739a14247a3ca4ec3edd13
JCIII <jcostello711@gmail.com>
2022-04-10 19:38:02 -0400

    comments can be added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
